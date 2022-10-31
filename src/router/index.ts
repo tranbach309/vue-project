fd7496ffb3f330d1ff19ef9cef697394537b76b4
tranbach309 <tranbach309@gmail.com>
2022-10-31 22:32:26 +0700

    Add new DemoView Component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DemoView from '../views/DemoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/demo',
+      name: 'demo',
+      component: DemoView
     },
     {
       path: '/about',
